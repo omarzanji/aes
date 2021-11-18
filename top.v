34ec0fc01a5b76e26abd43f7f66c7bee31a69cc3
Omar Barazanji <ozanji345@gmail.com>
2021-11-17 19:51:01 -0600

    added UART and confirmed AES rounds successful

--- top.v
+++ top.v
@@ -8,13 +8,33 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module top (
-    input wire [0 : 127] in,
-    input wire [0 : 4] round,
     input wire clk,
-    input wire [0 : 127] key,
-    output wire [0 : 127] key_out,
+    input wire debug_mode,
+//    input wire Rx_Serial,
     output reg [0 : 127] out
 );
+
+reg [0 : 127] in;
+reg [0 : 4] round = 0;
+reg [0 : 127] key = {
+    8'h2b,
+    8'h7e,
+    8'h15,
+    8'h16,
+    8'h28,
+    8'hae,
+    8'hd2,
+    8'ha6,
+    8'hab,
+    8'hf7,
+    8'h15,
+    8'h88,
+    8'h09,
+    8'hcf,
+    8'h4f,
+    8'h3c
+};
+wire [0 : 127] key_out;
 
 wire [0:7] b0;
 wire [0:7] b1;
@@ -42,7 +62,6 @@ assign sub_arr = {
 };
 
 // output wire
-wire [0 : 127] out_key_scheduler;
 reg [0 : 127] out_reg;
 
 sbox sbox1_top (.sbox_in(in[0+:8]), .sbox_out(b0));
@@ -62,6 +81,60 @@ sbox sbox14_top (.sbox_in(in[104+:8]), .sbox_out(b13));
 sbox sbox15_top (.sbox_in(in[112+:8]), .sbox_out(b14));
 sbox sbox16_top (.sbox_in(in[120+:8]), .sbox_out(b15));
 
+// uart parameters
+parameter c_CLOCK_PERIOD_NS = 100;
+parameter c_CLKS_PER_BIT    = 87;
+parameter c_BIT_PERIOD      = 8600;
+
+// uart signals
+reg r_Rx_Serial = 1;
+reg r_Tx_DV = 0;
+wire w_Tx_Done;
+reg [7:0] r_Tx_Byte = 0;
+wire [7:0] w_Rx_Byte;
+
+//assign Rx_Serial = r_Rx_Serial;
+
+uart_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_RX_INST
+(.i_Clock(clk),
+.i_Rx_Serial(r_Rx_Serial),
+.o_Rx_DV(),
+.o_Rx_Byte(w_Rx_Byte)
+);
+
+uart_tx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) UART_TX_INST
+(.i_Clock(clk),
+.i_Tx_DV(r_Tx_DV),
+.i_Tx_Byte(r_Tx_Byte),
+.o_Tx_Active(),
+.o_Tx_Serial(),
+.o_Tx_Done(w_Tx_Done)
+);
+
+task UART_WRITE_BYTE;
+    input [7:0] i_Data;
+    integer     ii;
+    begin
+       
+      // Send Start Bit
+      r_Rx_Serial <= 1'b0;
+      #(c_BIT_PERIOD);
+      #1000;
+       
+       
+      // Send Data Byte
+      for (ii=0; ii<8; ii=ii+1)
+        begin
+          r_Rx_Serial <= i_Data[ii];
+          #(c_BIT_PERIOD);
+        end
+       
+      // Send Stop Bit
+      r_Rx_Serial <= 1'b1;
+      #(c_BIT_PERIOD);
+     end
+  endtask // UART_WRITE_BYTE
+
 wire [0:127] shift_arr;
 shift_rows shift_rows_top (.shift_rows_in(sub_arr), .shift_rows_out(shift_arr));
 
@@ -69,19 +142,130 @@ wire [0:127] mix_arr;
 mix_col mix_col_top(.mix_col_in(shift_arr), .mix_col_out(mix_arr));
 
 // connect the sbox module to the in/out created above
-key_scheduler key_scheduler_tb(.round_in(round), .clk(clk), .key_in(key), .out(out_key_scheduler));
-assign key_out = out_key_scheduler;
+key_scheduler key_scheduler_tb(.round_in(round), .clk(clk), .key_in(key), .out(key_out));
 
-always @( clk) begin
-    case(round)
-        0:
-            out_reg = in ^ key;
-        10:
-            out_reg = shift_arr ^ out_key_scheduler;
-        default:
-            out_reg = mix_arr ^ out_key_scheduler;
+reg [2:0] state=0;
+//reg [0:127] uart_tx_test=32'hABCD;
+//reg uart_tx_reg;
+//reg uart_rst_reg=1;
+reg [0:8] counter=0;
+//assign uart_tx = uart_tx_reg;
+//assign uart_rst = uart_rst_reg;
+
+reg [20:0] clk_cnt=0;
+
+reg [0:127] debug_input  = {  
+    8'h32,
+    8'h43,
+    8'hf6,
+    8'ha8,
+    8'h88,
+    8'h5a,
+    8'h30,
+    8'h8d,
+    8'h31,
+    8'h31,
+    8'h98,
+    8'ha2,
+    8'he0,
+    8'h37,
+    8'h07,
+    8'h34
+};
+reg [0:127] debug_output  = {  
+    8'h39,
+    8'h25,
+    8'h84,
+    8'h1d,
+    8'h02,
+    8'hdc,
+    8'h09,
+    8'hfb,
+    8'hdc,
+    8'h11,
+    8'h85,
+    8'h97,
+    8'h19,
+    8'h6a,
+    8'h0b,
+    8'h32
+};
+
+reg correct_flag = 0;
+
+always @(posedge clk) begin
+//    r_Rx_Serial = Rx_Serial;
+    case (state)
+        0: // wait state
+            begin
+                if (debug_mode==1) begin
+                    state=1;
+                    clk_cnt=77399;
+                    UART_WRITE_BYTE(debug_input[counter+:8]);
+                end
+//                if (r_Rx_Serial==0) begin // wait for incoming msg
+//                    state=1; // go to incoming msg recv state
+//                end
+            end
+        1: // incoming msg recv state
+            begin
+                clk_cnt=clk_cnt+1;
+                if (clk_cnt==77400) begin // wait for downlink to finish
+                    in[counter+:8]=w_Rx_Byte; // set input to incoming msg
+                    state=2; // go to downlink finish state
+                end      
+            end
+        2: // downlink finish state
+            begin
+                counter=counter+1; // count until full, 128-bit input received
+                if (counter==121) begin 
+                    state=3; // go to recv finish state and begin AES rounds
+                    clk_cnt=0;
+                end
+                else begin
+                    state=0; // go back to checking for input
+                end
+            end
+        3: // begin AES rounds
+            begin
+                case(round)
+                    0:
+                        begin
+                            out_reg = in ^ key;
+                            round=round+1;
+                        end
+                    10:
+                        begin
+                            in = out_reg;
+                            key = key_out;
+                            out_reg = shift_arr ^ key_out;
+                            clk_cnt=clk_cnt+1;
+                            if (clk_cnt==2) begin
+                                clk_cnt=0;
+                                round=0;
+                                state=4; // go to AES done state
+                            end
+                        end
+                    default:
+                        begin
+                            key = key_out;
+                            in = out_reg;
+                            out_reg = mix_arr ^ key_out;
+                            clk_cnt=clk_cnt+1;
+                            if (clk_cnt==2) begin
+                                clk_cnt=0;
+                                round=round+1;
+                            end
+                        end
+                endcase
+            end
+        4:  // AES done state
+            begin
+                if (out_reg == debug_output) begin
+                    correct_flag=1;
+                end
+            end
     endcase
-
 out = out_reg;
 end
 endmodule
--- top_tb.v
+++ top_tb.v
@@ -10,199 +10,22 @@
 // initialize module top_tb
 module top_tb;
 
-// setting up sample inputs to test on 
-reg [0:127] sample_input  = {  
-    8'h32,
-    8'h43,
-    8'hf6,
-    8'ha8,
-    8'h88,
-    8'h5a,
-    8'h30,
-    8'h8d,
-    8'h31,
-    8'h31,
-    8'h98,
-    8'ha2,
-    8'he0,
-    8'h37,
-    8'h07,
-    8'h34
-};
 reg clk=0;
 wire [0:127] sample_output;
-wire [0:127] key_out_tb;
-reg [0:127] key_out_reg;
-
-reg [0 : 4] round_tb=0;
-reg [0 : 127] key_tb = {  
-    8'h2b,
-    8'h7e,
-    8'h15,
-    8'h16,
-    8'h28,
-    8'hae,
-    8'hd2,
-    8'ha6,
-    8'hab,
-    8'hf7,
-    8'h15,
-    8'h88,
-    8'h09,
-    8'hcf,
-    8'h4f,
-    8'h3c
-};
-
 reg [0 : 127] out_reg;
 
+reg debug_mode_tb = 1;
+
 // connect the sbox module to the in/out created above
-top top_tb(.in(sample_input), .clk(clk), .round(round_tb), .key(key_tb), .key_out(key_out_tb), .out(sample_output));
+top top_tb(.clk(clk), .out(sample_output), .debug_mode(debug_mode_tb));
+
 initial
 begin
 
     // monitor in/out in simulation
-    $monitor(sample_input, round_tb, key_tb, key_out_tb, sample_output);
+    $monitor();
     
     assign out_reg = sample_output;
-    assign key_out_reg = key_out_tb;
-    
-    #5 // round 0
-    clk = !clk;
-    
-    #5 // round 0
-    clk = !clk;
- 
-    #5 // round 1
-    round_tb = 1;
-    sample_input = out_reg;
-    key_tb = key_out_reg;
-    clk = !clk;
-    
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb =2;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb =3;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-    
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 4;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 5;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 6;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 7;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 8;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 9;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
-    
-    #5
-    round_tb = 10;
-    clk = !clk;
-    key_tb = key_out_reg;
-    sample_input = out_reg;
-
-    #5
-    clk = !clk;
-    #5 
-    clk = !clk;
-    #5 
-    clk = !clk;
     
 end
 endmodule
